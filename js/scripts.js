450bd5f29b5690cd2ebb5b63a2906b886faad8b7
Becky Heaton <becky.a.heaton@outlook.com>
2024-06-02 15:15:25 +1000

    Utilise .js external file

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // alert("The file is working");
+
+    // Select the element with the id 'title' and change its text content
+    document.getElementById('title').textContent = "I've always loved you";
+
+    
+});
